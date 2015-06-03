4fb8138947228f79dbe53eaafb02e8882ea5ed03
Maira Bello <maira.araujo@liferay.com>
2015-06-03 13:29:10 -0300

    Makes build output soy files as well

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 'use strict';
 
 var metalTasks = require('metal-tasks');
+var path = require('path');
 
 metalTasks({
 	buildSrc: 'bower_components/alloy-*/src/**/*.js',
 	bundleFileName: 'alloy.js',
 	globalName: 'alloy',
+	registerTestTasks: false,
 	scssSrc: 'bower_components/alloy-*/src/**/*.scss',
-	registerTestTasks: false
+	soyDest: 'bower_components/alloy-*/src',
+	soyGeneratedDest: function(file) {
+		var index = file.relative.indexOf(path.sep + 'src' + path.sep);
+		file.base = path.join(file.base, file.relative.substr(0, index + 5));
+		return 'build/soy';
+	},
+	soySrc: 'bower_components/alloy-*/src/**/*.soy'
 });
