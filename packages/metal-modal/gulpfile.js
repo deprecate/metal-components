e78146a6baa90cfa9a878d3fd25306e721e628e7
Maira Bello <maira.araujo@liferay.com>
2015-06-04 12:45:37 -0300

    Adds amd demo

--- packages/metal-modal/gulpfile.js
+++ packages/metal-modal/gulpfile.js
@@ -5,5 +5,6 @@ var metalTasks = require('metal-tasks');
 metalTasks({
 	bundleCssFileName: 'modal.css',
 	bundleFileName: 'modal.js',
-	globalName: 'alloy'
+	globalName: 'alloy',
+	moduleName: 'alloy-modal'
 });
