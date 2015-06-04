e4d416a01fe7bdf1064412f0b3b848c39b86b05d
Maira Bello <maira.araujo@liferay.com>
2015-06-04 13:40:24 -0300

    Sets option to skip soy compilation
    We don't need to compile soy files since they're already coming compiled from dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,11 @@ metalTasks({
 	globalName: 'alloy',
 	registerTestTasks: false,
 	scssSrc: 'bower_components/alloy-*/src/**/*.scss',
-	soyDest: 'bower_components/alloy-*/src',
 	soyGeneratedDest: function(file) {
 		var index = file.relative.indexOf(path.sep + 'src' + path.sep);
 		file.base = path.join(file.base, file.relative.substr(0, index + 5));
 		return 'build/soy';
 	},
+	soySkipCompilation: true,
 	soySrc: 'bower_components/alloy-*/src/**/*.soy'
 });
