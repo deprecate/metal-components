16c9144c5585b1d49f2037913be99ec2d98a5070
Maira Bello <maira.araujo@liferay.com>
2015-06-05 18:13:56 -0300

    Replaces metal and metal-tasks with gulp-metal

--- packages/metal-treeview/gulpfile.js
+++ packages/metal-treeview/gulpfile.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var metalTasks = require('metal-tasks');
+var metal = require('gulp-metal');
 
-metalTasks({
+metal.registerTasks({
 	bundleCssFileName: 'treeview.css',
 	bundleFileName: 'treeview.js',
 	globalName: 'alloy',
