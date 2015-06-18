4e3b6f82913049c21769870f4a7f27cb08c4f507
Maira Bello <maira.araujo@liferay.com>
2015-06-18 13:55:31 -0300

    Adds karma config files to run tests from all crystal components

---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+var metal = require('gulp-metal');
+
+var babelOptions = {
+	resolveModuleSource: metal.renameAlias,
+	sourceMap: 'both'
+};
+
+module.exports = function (config) {
+	config.set({
+		frameworks: ['mocha', 'chai', 'source-map-support', 'commonjs'],
+
+		files: [
+			'bower_components/soyutils/soyutils.js',
+			'bower_components/metal*/src/**/*.js',
+			'bower_components/crystal*/src/**/*.js',
+			'bower_components/crystal*/test/**/*.js'
+		],
+
+		preprocessors: {
+			'bower_components/metal*/src/**/*.js': ['babel', 'commonjs'],
+			'bower_components/crystal*/src/**/*.js': ['babel', 'commonjs'],
+			'bower_components/crystal*/test/**/*.js': ['babel', 'commonjs']
+		},
+
+		browsers: ['Chrome'],
+
+		babelPreprocessor: {options: babelOptions}
+	});
+}
