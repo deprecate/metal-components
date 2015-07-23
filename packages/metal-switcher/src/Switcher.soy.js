58ac3f8e66fafb6e566d544d0d4807192ccd46ce
Maira Bello <maira.araujo@liferay.com>
2015-07-23 15:58:31 -0300

    Makes elementClasses param optional

--- packages/metal-switcher/src/Switcher.soy.js
+++ packages/metal-switcher/src/Switcher.soy.js
@@ -19,12 +19,12 @@ if (typeof Templates.Switcher == 'undefined') { Templates.Switcher = {}; }
  * @suppress {checkTypes}
  */
 Templates.Switcher.content = function(opt_data, opt_ignored, opt_ijData) {
-  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '" class="switcher component ' + soy.$$escapeHtmlAttribute(opt_data.elementClasses) + '"><div class="switcher-control"><div class="switcher-control-icon"></div></div></div>');
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '" class="switcher component' + soy.$$escapeHtmlAttribute(opt_data.elementClasses ? ' ' + opt_data.elementClasses : '') + '"><div class="switcher-control"><div class="switcher-control-icon"></div></div></div>');
 };
 if (goog.DEBUG) {
   Templates.Switcher.content.soyTemplateName = 'Templates.Switcher.content';
 }
 
-Templates.Switcher.content.params = ["elementClasses","id"];
+Templates.Switcher.content.params = ["id"];
 export default Templates.Switcher;
 /* jshint ignore:end */
