bbec00824c4eb0d866283d37135a0bbaab57648c
Maira Bello <maira.araujo@liferay.com>
2015-09-10 11:04:36 -0300

    Adds crystal-slider

--- karma-coverage.conf.js
+++ karma-coverage.conf.js
@@ -6,6 +6,10 @@ var babelOptions = {
 	sourceMap: 'both'
 };
 
+var scssOptions = {
+	sourceMap: true
+};
+
 module.exports = function (config) {
 	config.set({
 		frameworks: ['mocha', 'chai', 'source-map-support', 'commonjs'],
@@ -13,15 +17,18 @@ module.exports = function (config) {
 		files: [
 			'bower_components/soyutils/soyutils.js',
 			'bower_components/metal*/src/**/*.js',
+			'bower_components/metal-drag-drop/test/fixtures/DragTestHelper.js',
 			'bower_components/crystal*/src/**/*.js',
-			'bower_components/crystal*/test/**/*.js'
+			'bower_components/crystal*/test/**/*.js',
+			'bower_components/crystal*/src/**/*.scss'
 		],
 
 		preprocessors: {
 			'bower_components/crystal*/src/**/!(*.soy).js': ['coverage', 'commonjs'],
 			'bower_components/crystal*/src/**/*.soy.js': ['babel', 'commonjs'],
-			'bower_components/metal*/src/**/*.js': ['babel', 'commonjs'],
-			'bower_components/crystal*/test/**/*.js': ['babel', 'commonjs']
+			'bower_components/metal*/**/*.js': ['babel', 'commonjs'],
+			'bower_components/crystal*/test/**/*.js': ['babel', 'commonjs'],
+			'bower_components/crystal*/src/**/*.scss': ['scss']
 		},
 
 		browsers: ['Chrome'],
@@ -29,6 +36,8 @@ module.exports = function (config) {
 		reporters: ['coverage', 'progress'],
 
 		babelPreprocessor: {options: babelOptions},
+
+		scssPreprocessor: {options: scssOptions},
 
 		coverageReporter: {
 			instrumenters: {isparta : isparta},
--- karma.conf.js
+++ karma.conf.js
@@ -5,6 +5,10 @@ var babelOptions = {
 	sourceMap: 'both'
 };
 
+var scssOptions = {
+	sourceMap: true
+};
+
 module.exports = function (config) {
 	config.set({
 		frameworks: ['mocha', 'chai', 'source-map-support', 'commonjs'],
@@ -12,18 +16,23 @@ module.exports = function (config) {
 		files: [
 			'bower_components/soyutils/soyutils.js',
 			'bower_components/metal*/src/**/*.js',
+			'bower_components/metal-drag-drop/test/fixtures/DragTestHelper.js',
 			'bower_components/crystal*/src/**/*.js',
-			'bower_components/crystal*/test/**/*.js'
+			'bower_components/crystal*/test/**/*.js',
+			'bower_components/crystal*/src/**/*.scss'
 		],
 
 		preprocessors: {
-			'bower_components/metal*/src/**/*.js': ['babel', 'commonjs'],
+			'bower_components/metal*/**/*.js': ['babel', 'commonjs'],
 			'bower_components/crystal*/src/**/*.js': ['babel', 'commonjs'],
-			'bower_components/crystal*/test/**/*.js': ['babel', 'commonjs']
+			'bower_components/crystal*/test/**/*.js': ['babel', 'commonjs'],
+			'bower_components/crystal*/src/**/*.scss': ['scss']
 		},
 
 		browsers: ['Chrome'],
 
-		babelPreprocessor: {options: babelOptions}
+		babelPreprocessor: {options: babelOptions},
+
+		scssPreprocessor: {options: scssOptions}
 	});
 }
