dbea6c09da339ead3310f7d02f65a3c185462a88
Maira Bello <maira.araujo@liferay.com>
2015-11-26 14:05:11 -0300

    Adds soy files to the build

--- gulpfile.js
+++ gulpfile.js
@@ -12,4 +12,9 @@ metal.registerTasks({
 	scssSrc: 'bower_components/crystal-*/src/**/*.scss'
 });
 
-gulp.task('default', ['build:globals', 'build:amd', 'build:amd:jquery']);
+gulp.task('soy:copy', function() {
+	return gulp.src('bower_components/crystal-*/src/**/*.soy')
+		.pipe(gulp.dest('build/soy'))
+});
+
+gulp.task('default', ['build:globals', 'build:amd', 'build:amd:jquery', 'soy:copy']);
