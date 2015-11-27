d758aee62efbd31d2c87334f183265061fc86f1b
Maira Bello <maira.araujo@liferay.com>
2015-11-27 13:30:26 -0300

    Fixes lint errors

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 var gulp = require('gulp');
 var metal = require('gulp-metal');
-var path = require('path');
 
 metal.registerTasks({
 	buildSrc: 'bower_components/crystal-*/src/**/*.js',
@@ -14,7 +13,7 @@ metal.registerTasks({
 
 gulp.task('soy:copy', function() {
 	return gulp.src('bower_components/crystal-*/src/**/*.soy')
-		.pipe(gulp.dest('build/soy'))
+		.pipe(gulp.dest('build/soy'));
 });
 
 gulp.task('default', ['build:globals', 'build:amd', 'build:amd:jquery', 'soy:copy']);
