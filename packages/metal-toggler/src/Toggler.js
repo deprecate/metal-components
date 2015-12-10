5486d592c39925af399b6ea04236cddc88722675
Maira Bello <maira.araujo@liferay.com>
2015-12-10 15:49:30 -0300

    Adds documentation

--- packages/metal-toggler/src/Toggler.js
+++ packages/metal-toggler/src/Toggler.js
@@ -5,7 +5,13 @@ import dom from 'bower:metal/src/dom/dom';
 import Attribute from 'bower:metal/src/attribute/Attribute';
 import EventHandler from 'bower:metal/src/events/EventHandler';
 
+/**
+ * Toggler component.
+ */
 class Toggler extends Attribute {
+	/**
+	 * @inheritDoc
+	 */
   constructor(opt_config) {
     super(opt_config);
 
@@ -18,13 +24,22 @@ class Toggler extends Attribute {
     this.syncHeader();
   }
 
-  handleKeydown(event) {
+  /**
+   * Handles a `keydown` event on the header.
+   * @param {!Event} event
+   * @protected
+   */
+  handleKeydown_(event) {
     if (event.keyCode === 13 || event.keyCode === 32) {
       this.toggle();
       event.preventDefault();
     }
   }
 
+  /**
+   * Syncs the component according to the value of the `expanded` attribute,
+   * updating the css class of the `content` element.
+   */
   syncExpanded() {
     if (this.expanded) {
       dom.addClasses(this.content, Toggler.CSS_EXPANDED);
@@ -35,38 +50,67 @@ class Toggler extends Attribute {
     }
   }
 
+  /**
+   * Syncs the component according to the value of the `header` attribute,
+   * attaching events to the new element and detaching from any previous one.
+   */
   syncHeader() {
     this.headerEventHandler_.removeAllListeners();
     if (this.header) {
       this.headerEventHandler_.add(
         dom.on(this.header, 'click', this.toggle.bind(this)),
-        dom.on(this.header, 'keydown', this.handleKeydown.bind(this))
+        dom.on(this.header, 'keydown', this.handleKeydown_.bind(this))
       );
     }
   }
 
+  /**
+   * Toggles the content's visibility.
+   */
   toggle() {
     this.expanded = !this.expanded;
   }
 }
 
+/**
+ * Attributes configuration.
+ */
 Toggler.ATTRS = {
+  /**
+   * The element that should be expanded/collapsed by this toggler.
+   * @type {string|!Element}
+   */
   content: {
     setter: dom.toElement,
     validator: value => core.isString(value) || core.isElement(value)
   },
 
+  /**
+   * Flag indicating if the content is currently expanded or collapsed.
+   * @type {boolean}
+   */
   expanded: {
     value: false
   },
 
+  /**
+   * The element that should be trigger toggling.
+   * @type {string|!Element}
+   */
   header: {
     setter: dom.toElement,
     validator: value => core.isString(value) || core.isElement(value)
   }
 };
 
-Toggler.CSS_EXPANDED = 'toggler-expanded';
+/**
+ * The CSS class added to the content when it's collapsed.
+ */
 Toggler.CSS_COLLAPSED = 'toggler-collapsed';
+
+/**
+ * The CSS class added to the content when it's expanded.
+ */
+Toggler.CSS_EXPANDED = 'toggler-expanded';
 
 export default Toggler;
