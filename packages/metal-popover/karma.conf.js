f2f35b2889df2c2836f637798d496e02025b1def
Maira Bello <maira.araujo@liferay.com>
2015-12-14 11:29:59 -0300

    Removes unnecessary karma config files

--- packages/metal-popover/karma.conf.js
+++
@@ -1,8 +0,0 @@
-var metalKarmaConfig = require('metal-karma-config');
-
-module.exports = function (config) {
-	metalKarmaConfig(config);
-
-	config.files.push('bower_components/crystal-*/src/**/*.js');
-	config.preprocessors['bower_components/crystal-*/**/*.js'] = ['babel', 'commonjs'];
-}
