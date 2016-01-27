69aaad3005d03b2ac94618e93ad80c42243150c0
Maira Bello <maira.araujo@liferay.com>
2016-01-27 11:18:28 -0300

    Stops using bower

--- packages/metal-progressbar/src/ProgressBar.js
+++ packages/metal-progressbar/src/ProgressBar.js
@@ -1,7 +1,7 @@
 'use strict';
 
-import core from 'bower:metal/src/core';
-import dom from 'bower:metal/src/dom/dom';
+import core from 'metal/src/core';
+import dom from 'metal/src/dom/dom';
 import ProgressBarBase from './ProgressBar.soy';
 
 /**
