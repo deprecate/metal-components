5f869930958ffad0aecdf986ebb87f74922fbcef
Maira Bello <maira.araujo@liferay.com>
2016-02-25 14:52:02 -0300

    Upgrades version of gulp-metal to 0.14.0

--- packages/metal-dropdown/gulpfile.js
+++ packages/metal-dropdown/gulpfile.js
@@ -5,6 +5,5 @@ var metal = require('gulp-metal');
 metal.registerTasks({
 	bundleCssFileName: 'dropdown.css',
 	bundleFileName: 'dropdown.js',
-	mainBuildJsTasks: ['build:globals'],
 	moduleName: 'metal-dropdown'
 });
