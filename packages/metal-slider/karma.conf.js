c5950945ac31cf7bfd51e916ec34b29fc4d95679
Maira Bello <maira.araujo@liferay.com>
2016-03-31 16:30:13 -0300

    Fixes karma tests

--- packages/metal-slider/karma.conf.js
+++ packages/metal-slider/karma.conf.js
@@ -5,7 +5,7 @@ var metalKarmaConfig = require('metal-karma-config');
 module.exports = function (config) {
 	metalKarmaConfig(config);
 
-	config.plugins.push('karma-scss-preprocessor');
+	config.plugins.push(require('karma-scss-preprocessor'));
 	config.files.push(
 		'node_modules/metal-drag-drop/test/fixtures/DragTestHelper.js',
 		'src/**/*.scss'
