455cfd4d62112745d2c33b2e21e8aa490de989af
Maira Bello <maira.araujo@liferay.com>
2016-04-07 13:44:43 -0300

    Stops using the removed "render" function

--- packages/metal-progressbar/test/ProgressBar.js
+++ packages/metal-progressbar/test/ProgressBar.js
@@ -14,17 +14,17 @@ describe('ProgressBar', function() {
 		it('should render specified label', function() {
 			progressBar = new ProgressBar({
 				label: 'My Label'
-			}).render();
+			});
 			assert.strictEqual('My Label', progressBar.element.textContent);
 		});
 
 		it('should not render any text if no label is specified', function() {
-			progressBar = new ProgressBar().render();
+			progressBar = new ProgressBar();
 			assert.strictEqual('', progressBar.element.textContent);
 		});
 
 		it('should update the rendered label when the state changes', function(done) {
-			progressBar = new ProgressBar().render();
+			progressBar = new ProgressBar();
 
 			progressBar.label = 'My Label';
 			progressBar.once('stateSynced', function() {
@@ -36,14 +36,14 @@ describe('ProgressBar', function() {
 
 	describe('Size', function() {
 		it('should render bar with 0% width by default', function() {
-			progressBar = new ProgressBar().render();
+			progressBar = new ProgressBar();
 			assert.strictEqual('0%', progressBar.element.childNodes[0].style.width);
 		});
 
 		it('should render bar with the correct width according to `value`', function() {
 			progressBar = new ProgressBar({
 				value: 60
-			}).render();
+			});
 			assert.strictEqual('60%', progressBar.element.childNodes[0].style.width);
 		});
 
@@ -52,7 +52,7 @@ describe('ProgressBar', function() {
 				max: 350,
 				min: 150,
 				value: 250
-			}).render();
+			});
 			assert.strictEqual('50%', progressBar.element.childNodes[0].style.width);
 		});
 
@@ -61,7 +61,7 @@ describe('ProgressBar', function() {
 				max: 350,
 				min: 150,
 				value: 250
-			}).render();
+			});
 
 			progressBar.value = 300;
 			progressBar.once('stateSynced', function() {
@@ -75,7 +75,7 @@ describe('ProgressBar', function() {
 				max: 350,
 				min: 150,
 				value: 250
-			}).render();
+			});
 
 			progressBar.min = 200;
 			progressBar.once('stateSynced', function() {
@@ -89,7 +89,7 @@ describe('ProgressBar', function() {
 				max: 350,
 				min: 150,
 				value: 250
-			}).render();
+			});
 
 			progressBar.max = 300;
 			progressBar.once('stateSynced', function() {
@@ -103,7 +103,7 @@ describe('ProgressBar', function() {
 				max: 350,
 				min: 150,
 				value: 250
-			}).render();
+			});
 
 			progressBar.max = 200;
 			progressBar.once('stateSynced', function() {
@@ -120,7 +120,7 @@ describe('ProgressBar', function() {
 				max: 350,
 				min: 150,
 				value: 250
-			}).render();
+			});
 
 			progressBar.min = 300;
 			progressBar.once('stateSynced', function() {
@@ -137,7 +137,7 @@ describe('ProgressBar', function() {
 				max: 350,
 				min: 150,
 				value: 100
-			}).render();
+			});
 			assert.strictEqual(150, progressBar.value);
 
 			progressBar.value = 400;
@@ -149,14 +149,14 @@ describe('ProgressBar', function() {
 		it('should add the specified class to the inner bar element', function() {
 			progressBar = new ProgressBar({
 				barClass: 'progress-bar-danger'
-			}).render();
+			});
 			assert.ok(dom.hasClass(progressBar.element.childNodes[0], 'progress-bar-danger'));
 		});
 
 		it('should update the inner bar element\'s class when `barClass` changes', function(done) {
 			progressBar = new ProgressBar({
 				barClass: 'progress-bar-danger'
-			}).render();
+			});
 
 			progressBar.barClass = 'progress-bar-info';
 			progressBar.once('stateSynced', function() {
@@ -169,14 +169,14 @@ describe('ProgressBar', function() {
 
 	describe('ARIA', function() {
 		it('should have the progressbar role', function() {
-			progressBar = new ProgressBar().render();
+			progressBar = new ProgressBar();
 			assert.strictEqual('progressbar', progressBar.element.getAttribute('role'));
 		});
 
 		it('should set and update the aria-valuenow ARIA attribute', function(done) {
 			progressBar = new ProgressBar({
 				value: 20
-			}).render();
+			});
 			assert.strictEqual('20', progressBar.element.getAttribute('aria-valuenow'));
 
 			progressBar.value = 40;
@@ -189,7 +189,7 @@ describe('ProgressBar', function() {
 		it('should set and update the aria-valuemin ARIA attribute', function(done) {
 			progressBar = new ProgressBar({
 				min: 20
-			}).render();
+			});
 			assert.strictEqual('20', progressBar.element.getAttribute('aria-valuemin'));
 
 			progressBar.min = 40;
@@ -202,7 +202,7 @@ describe('ProgressBar', function() {
 		it('should set and update the aria-valuemax ARIA attribute', function(done) {
 			progressBar = new ProgressBar({
 				max: 20
-			}).render();
+			});
 			assert.strictEqual('20', progressBar.element.getAttribute('aria-valuemax'));
 
 			progressBar.max = 40;
