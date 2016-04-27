0cddd593df1dd95d04c57c584a9c5dcd9c3c4c99
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-04-27 13:26:51 +0200

    Adds project files

---
+++ packages/metal-pagination/gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var metal = require('gulp-metal');
+
+metal.registerTasks({
+	bundleCssFileName: 'pagination.css',
+	bundleFileName: 'pagination.js',
+	moduleName: 'metal-pagination'
+});
