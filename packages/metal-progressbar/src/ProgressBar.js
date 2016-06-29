f91c76c13e8a92bea7e513fabb972adcb72d2489
Maira Bello <maira.araujo@liferay.com>
2016-06-29 15:10:13 -0300

    Adds ".js" extension to soy import to avoid conflicts
    This can conflict with the soy file when using tools like browserify to build the code.

--- packages/metal-progressbar/src/ProgressBar.js
+++ packages/metal-progressbar/src/ProgressBar.js
@@ -3,7 +3,7 @@
 import core from 'metal';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
-import templates from './ProgressBar.soy';
+import templates from './ProgressBar.soy.js';
 
 /**
  * UI Component that renders a progress bar.
