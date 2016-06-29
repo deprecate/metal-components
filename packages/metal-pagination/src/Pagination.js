b0e5aa0e31e6914a4e11bb3a2489320a49c03c01
Maira Bello <maira.araujo@liferay.com>
2016-06-29 15:06:34 -0300

    Adds ".js" extension to soy import to avoid conflicts
    This can conflict with the soy file when using tools like browserify to build the code.

--- packages/metal-pagination/src/Pagination.js
+++ packages/metal-pagination/src/Pagination.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import core from 'metal';
-import templates from './Pagination.soy';
+import templates from './Pagination.soy.js';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
 
